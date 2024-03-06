4d2ff8b454b8a9e55272a854a6d4f6e49505b617
Gaurav <118659860+Gaurav153fr@users.noreply.github.com>
2024-03-06 04:04:27 +0000

    added key

--- components/epilist.tsx
+++ components/epilist.tsx
@@ -9,7 +9,7 @@ export default function EpiList(props: Props) {
     return (
         <section className=" p-5 grid grid-cols-8 gap-x-2">
             {props.items.map((item, i) => (
-                <Link href={"/watch/1/goku/1"} className={cn(buttonVariants({variant:'secondary'})," " )}>{i}</Link>
+                <Link key={i} href={"/watch/1/goku/1"} className={cn(buttonVariants({variant:'secondary'})," " )}>{i}</Link>
 
             ))}
         </section>
