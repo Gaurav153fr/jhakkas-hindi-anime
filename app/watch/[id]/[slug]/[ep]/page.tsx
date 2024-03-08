abb39640f0a9a62fca1e6e234dd8fffaee30563f
Gaurav153fr <118659860+Gaurav153fr@users.noreply.github.com>
2024-03-08 17:34:45 +0530

    fixed small vide player

--- app/watch/[id]/[slug]/[ep]/page.tsx
+++ app/watch/[id]/[slug]/[ep]/page.tsx
@@ -15,7 +15,7 @@ export default function Home({ params }: { params: params }) {
   return (
     <main className="flex flex-col items-center justify-between">
       <section className="flex md:flex-row flex-col w-screen h-screen">
-        <div className="md:w-3/5 w-full h-fit aspect-video md:p-5">
+        <div className="md:w-3/5 w-full min-h-[210px] aspect-video md:p-5">
           <VideoIframe id={params.id} ep_no={params.ep} />
         </div>
         <div className="md:w-2/5 w-full h-full md:px-0 px-2">
